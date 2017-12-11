dedc79ab2cf1aa7cce6aa23129b90cd6df1f93d7
Syed Wajahat Hussain <wajahat.hussain@vadion.com>
2017-12-11 17:30:07 +0500

    Home Screen Layout Addition, just initial overview.

--- MainHuNa/app/src/main/java/com/idealtechnologies/app/mainhuna/SplashActivity.java
+++ MainHuNa/app/src/main/java/com/idealtechnologies/app/mainhuna/SplashActivity.java
@@ -1,19 +1,23 @@
 package com.idealtechnologies.app.mainhuna;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.idealtechnologies.app.mainhuna.activities.MainActivity;
+import com.idealtechnologies.app.mainhuna.activities.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Intent intent = new Intent(this,MainActivity.class);
+        Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
         startActivity(intent);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         finish();
 
     }
